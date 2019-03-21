a31ccd075116897e0a8e6cdb82b0b44d96d7fcbd
Luca Tringali <tringalinvent@libero.it>
2019-03-21 23:46:07 +0100

    Clean debug strings

--- meteo/meteo.ino
+++ meteo/meteo.ino
@@ -11,16 +11,17 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 IPAddress server(192,168,0,103);  // numeric IP
-//char server[] = "raspberrypi.local";    // name address
 
 const char* host = "192.168.0.103";
 
 unsigned long towait = (1000UL*60*10);
-unsigned long iter = towait - 1000UL*10;
+//unsigned long iter = towait - 1000UL*10;
 
 const float drop = (1.8/(11*5))*10; // (9ml/5click)/(area in cm2)=cm of rain Note: 1ml = 1cm3
 
 bool debug = false;
+bool usenetwork = true;
+
 bool firstrun = true;
 
 float rain = 0.0;
@@ -57,6 +58,7 @@ void(* resetFunc) (void) = 0;
 void setup() {
     // Open serial communications and wait for port to open:
     Serial.begin(9600);
+    if (usenetwork) {
     if (debug) Serial.println("Looking for network...");
     // start the Ethernet connection:
     if (Ethernet.begin(mac) == 0) {
@@ -71,18 +73,17 @@ void setup() {
     Serial.println(Ethernet.localIP());
     // give the Ethernet shield a second to initialize:
     delay(1000);
+    }
     
     if (!bmp.begin()) {
         Serial.println("Could not find a valid BMP085 sensor");
         delay(2000);
     }
-    if (debug) Serial.println("BMP085 found");
     pinMode(RAINPIN, INPUT);
     for (int i = 0; i < 60; i++) rainV[i] = 0.0;
     sensor_t sensor;
     dht.temperature().getSensor(&sensor);
     dht.humidity().getSensor(&sensor);
-    if (debug) Serial.println("DHT sensor ready");
     delayMS = sensor.min_delay / 1000;
     delay(delayMS);
     sensors.begin(); 
@@ -106,45 +107,38 @@ void loop() {
     }
     
     if ((millis() % towait) == 0 || firstrun == true) {
-        if (debug) Serial.println("Reading sensors");
-        firstrun = false;
+        
         float pressure = 0.0;
         pressure = bmp.readPressure()/100;
         if (debug) {
-            Serial.print("Pressure = ");
-            Serial.print(pressure);
-            Serial.println(" hPa");
+            Serial.println(pressure);
         }
+
+        sensors_event_t event;
         
         float temperature = 0.0;
-        sensors_event_t event;  
-        dht.temperature().getEvent(&event);
+        /*dht.temperature().getEvent(&event);
         if (!isnan(event.temperature)) {
             temperature = (bmp.readTemperature()+event.temperature)/2;
-            if (debug) {
-                Serial.print("Temperature: ");
-                Serial.print(temperature);
-                Serial.println(" °C");
-            }
-        }
+        }*/
         sensors.requestTemperatures();
         temperature = sensors.getTempCByIndex(0);
+        if (debug) {
+            Serial.println(temperature);
+        }
         
         float humidity = 0.0;
         dht.humidity().getEvent(&event);
         if (!isnan(event.relative_humidity)) {
             humidity = event.relative_humidity;
-            if (debug) {
-                Serial.print("Humidity: ");
-                Serial.print(humidity);
-                Serial.println("%");
-            }
+        }
+        if (debug) {
+            Serial.println(humidity);
         }
         
         float tmprain = 0.0;
         for (int i =0 ; i < 60; i++) tmprain = tmprain + rainV[i];
         if (debug) {
-            Serial.print("Rain mm/h: ");
             Serial.println(tmprain);
         }
         
@@ -153,35 +147,27 @@ void loop() {
         }
         // Make a HTTP request:
         if (debug) Serial.println("Connecting to server");
+        if (usenetwork) {
         if (client.connect(server, 80)) {
             String pval = String(pressure, DEC);
             String tval = String(temperature, DEC);
             String hval = String(humidity, DEC);
             String rval = String(tmprain, DEC);
-            client.println("GET /meteo/write-values.php?temperature=" + tval + "&pressure=" + pval + "&humidity=" + hval + "&rain=" + rval + " HTTP/1.1");
+            String frval = String("");
+            if (firstrun) frval = String("&firstrun=1");
+            client.println("GET /meteo/write-values.php?temperature=" + tval + "&pressure=" + pval + "&humidity=" + hval + "&rain=" + rval + frval + " HTTP/1.1");
             client.println("Host: " + String(host));
             client.println("Connection: keep-open");
             client.println();
-            if (debug) {
-                Serial.print("GET /meteo/write-values.php?temperature=");
-                Serial.print(tval);
-                Serial.print("&pressure=");
-                Serial.print(pval);
-                Serial.print("&humidity=");
-                Serial.print(hval);
-                Serial.print("&rain=");
-                Serial.print(rval);
-                Serial.println(" HTTP/1.1");
-            }
-            if (debug) Serial.println("Data sent to server");
+            if (debug) Serial.println("Data sent");
         } else {
             Serial.println("Connection failed");
             client.stop();
             delay(1000);
             resetFunc();
         }
-        
-        //iter = 0;
+        }
+        firstrun = false;
     }
     
 }
