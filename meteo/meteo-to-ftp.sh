b23b8d6cf46ded295417a7f409a8af6897bfd8fd
zorbaproject <TRINGALINVENT@libero.it>
2019-06-12 21:59:45 +0200

    Write firstrun value in log file

--- meteo/meteo-to-ftp.sh
+++ meteo/meteo-to-ftp.sh
@@ -2,7 +2,7 @@
 #crontab -e
 #0,10,20,30,40,50 * * * * /home/pi/meteo-to-ftp.sh
 HOST='ftp.kraskikrti.net'
-HOST='89.46.104.211'
+#HOST='89.46.104.211'
 USER='1226041@aruba.it'
 PASSWD=$(cat passwd.txt)
 FOLDER='www.kraskikrti.net/meteo/'
--- meteo/write-values.php
+++ meteo/write-values.php
@@ -4,7 +4,8 @@ $temperature = $_GET['temperature'];
 $humidity = $_GET['humidity'];
 $pressure = $_GET['pressure'];
 $rain = $_GET['rain'];
-$txt = date("Y-m-d H:i:s").",".$temperature.",".$pressure.",".$humidity.",".$rain;
+$firstrun = $_GET['firstrun'];
+$txt = date("Y-m-d H:i:s").",".$temperature.",".$pressure.",".$humidity.",".$rain.",".firstrun;
 $myfile = file_put_contents('logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);  
 ?>
 
