4a0d59b240b256ee7e9a51c113d8a09069e9887c
Luca Tringali <tringalinvent@libero.it>
2020-04-05 10:31:35 +0200

    Using datalogging shield

--- conduttimetro-da-campo/conduttimetro-da-campo.ino
+++ conduttimetro-da-campo/conduttimetro-da-campo.ino
@@ -7,6 +7,7 @@
  *  EC Module code:
  * https://wiki.dfrobot.com/Gravity__Analog_Electrical_Conductivity_Sensor___Meter_V2__K=1__SKU_DFR0300
  * Copyright   [DFRobot](http://www.dfrobot.com), 2018
+ * Version 1: https://www.dfrobot.com/wiki/index.php/Analog_EC_Meter_SKU:DFR0300
  * 
  * LCD panel and buttons:
  * https://wiki.dfrobot.com/Arduino_LCD_KeyPad_Shield__SKU__DFR0009_
@@ -19,6 +20,19 @@
  ** MISO - pin 12
  ** CLK - pin 13
  ** CS - pin 4
+ *  
+ *  
+ * Requirements:
+ * EEPROM by Arduino
+ * Wire by Arduino
+ * SD by Arduino
+ * SPI by Arduino
+ * DFRobot_EC-master on https://github.com/DFRobot/DFRobot_EC
+ * DallasTemperature by Miles Burton et al
+ * OneWire by Jim Studt et al
+ * LiquidCrystal by Arduino, Adafruit
+ * RTClib by Adafruit
+ * 
  */
 
 #include "DFRobot_EC.h"
@@ -28,16 +42,17 @@
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <SD.h>
+#include "RTClib.h"
 
+#define buttonsPin A0
 #define EC_PIN A1
 #define ONE_WIRE_BUS 2 
-const int chipSelect = 4;
+#define chipSelect 10
 
 bool datalog = false;
 bool menu1 = false;
 
 int lcd_key     = 0;
-int adc_key_in  = 0;
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -46,22 +61,24 @@ int adc_key_in  = 0;
 #define btnNONE   5
 
 // select the pins used on the LCD panel
-LiquidCrystal lcd(8, 9, 3, 5, 6, 7);
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+#define KVALUEADDR 0x0A
 
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire);
 
-unsigned long logcount = 0;
 float voltage,ecValue,temperature = 25;
-const float defaultTemp = 19.0;
+const float defaultTemp = 20.0;
 
+unsigned long logcount = 0;
 int ecOldLen = 10;
 float* ecOld;
 int cycleiter = 0;
 
 DFRobot_EC ec;
 
-#define KVALUEADDR 0x0A
+RTC_DS1307 rtc;
 
 
 void setup()
@@ -77,10 +94,18 @@ void setup()
     lcd.setCursor(0,0);
     lcd.setCursor(12,0);
     lcd.print("noSD");
-    Serial.println("Card not found");
     delay(2000);
   }
   resizeEcOld();
+
+  rtc.begin();
+
+  //Set time at compilation time, if not already set
+  DateTime comp = DateTime(F(__DATE__), F(__TIME__));
+  DateTime now = rtc.now();
+  if (comp.unixtime()> now.unixtime()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop()
@@ -126,9 +151,13 @@ void loop()
       if (datalog) {
         File dataFile = SD.open("datalog.txt", FILE_WRITE);
         if (dataFile) {
-          String tval = String(temperature, DEC);
-          String cval = String(ecT, DEC);
-          dataFile.println(String(logcount)+String(",")+tval+String(",")+cval);
+          dataFile.print(String(logcount));
+          dataFile.print(",");
+          dataFile.print(String(rtc.now().timestamp(DateTime::TIMESTAMP_FULL)));
+          dataFile.print(",");
+          dataFile.print(String(temperature, DEC));
+          dataFile.print(",");
+          dataFile.println(String(ecT, DEC));
           dataFile.close();
           lcd.setCursor(12,0);
           lcd.print(" LOG");
@@ -144,7 +173,6 @@ void loop()
       lcd_key = read_LCD_buttons();  // read the buttons
       if (lcd_key==btnSELECT) menu1=true;
     }
-    //ec.calibration(voltage,temperature);          // calibration process by Serail CMD
 }
 
 float readTemperature()
@@ -158,7 +186,8 @@ float readTemperature()
 // read the buttons
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor
+ //int adc_key_in  = 0;
+ int adc_key_in = analogRead(buttonsPin);      // read the value from the sensor
  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
  // we add approx 50 to those values and check to see if we are close
  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
@@ -189,7 +218,7 @@ void menu()
   lcd.setCursor(0,1);
   lcd.print("                    ");
   int pos = 0;
-  int maxoptions = 5;
+  int maxoptions = 6;
   delay(1000);
   while (menu1) {
     lcd_key = read_LCD_buttons();  // read the buttons
@@ -231,16 +260,32 @@ void menu()
       lcd.print(ecOldLen);
     }
     if (abs(pos)%maxoptions == 2) {
+      DateTime now = rtc.now();
+      lcd.setCursor(0,1);
+      //lcd.print(now.timestamp(DateTime::TIMESTAMP_FULL));
+      lcd.print(now.timestamp(DateTime::TIMESTAMP_TIME));
+      //lcd.print(now.timestamp(DateTime::TIMESTAMP_DATE));
+      lcd.setCursor(12,1);
+      lcd.print(now.year());
+      lcd.setCursor(10,1);
+      if (now.day()<10) lcd.print("0");
+      lcd.print(now.day());
+      lcd.setCursor(12,1);
+      if (now.month()<10) lcd.print("0");
+      lcd.print(now.month());
+      if (lcd_key==btnSELECT) setDateTime();
+    }
+    if (abs(pos)%maxoptions == 3) {
       lcd.setCursor(0,1);
       lcd.print("Calibrazione      ");
       if (lcd_key==btnSELECT) calibrate();
     }
-    if (abs(pos)%maxoptions == 3) {
+    if (abs(pos)%maxoptions == 4) {
       lcd.setCursor(0,1);
       lcd.print("Reset memoria      ");
       if (lcd_key==btnSELECT) resetMemory();
     }
-    if (abs(pos)%maxoptions == 4) {
+    if (abs(pos)%maxoptions == 5) {
       lcd.setCursor(0,1);
       lcd.print("Esci               ");
       if (lcd_key==btnSELECT) menu1=false;
@@ -260,13 +305,21 @@ void resizeEcOld()
   }
 }
 
-void resetMemory() {
-  /*for(byte i = 0;i< 8; i   ){
-    EEPROM.write(KVALUEADDR i, 0xFF);
-  }*/
+void setDateTime() 
+{
+  // December 16, 2020 at 10pm you would call:
+  rtc.adjust(DateTime(2019, 12, 16, 22, 0, 0));
 }
 
-void calibrate() {
+void resetMemory() 
+{
+  for(byte i = 0;i< 8; i   ){
+    //EEPROM.write(KVALUEADDR i, 0xFF);
+  }
+}
+
+void calibrate() 
+{
   bool active = true;
   while (active) {
     static unsigned long timepoint = millis();
